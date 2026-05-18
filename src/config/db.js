194a92bec4fc9a669602593bd8947c5b5fecf369
Juanpedrogomezespinosa <juanpedrogomezespinosa@gmail.com>
2026-05-18 11:06:18 +0200

    cambio conexión base de datos

--- src/config/db.js
+++ src/config/db.js
@@ -6,19 +6,21 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT, // Añadimos esta línea
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  // Le decimos que si estamos en producción (leyendo la variable DB_SSL), active la conexión segura
+  ssl:
+    process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : undefined,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
-// Comprobamos la conexión automáticamente al arrancar
 try {
   const connection = await pool.getConnection();
-  console.log("📦 Conectado con éxito a la base de datos MySQL (tita_cosi_db)");
+  console.log(`📦 Conectado a la base de datos: ${process.env.DB_NAME}`);
   connection.release();
 } catch (error) {
   console.error("❌ Error conectando a la base de datos:", error.message);
